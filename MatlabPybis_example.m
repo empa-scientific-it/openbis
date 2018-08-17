1dc872a8d0f8fa1a297e4b1a7543228fcb364a8f
Henry Luetcke <hluetcke@ethz.ch>
2018-08-17 13:22:26 +0200

    extend example script

--- MatlabPybis_example.m
+++ MatlabPybis_example.m
@@ -1,5 +1,8 @@
 % demo script for accessing openBIS from Matlab using Pybis
 % we use Matlab's capability to call Python libraries
+% Pybis documentation is available here:
+% https://sissource.ethz.ch/sispub/pybis/blob/master/src/python/PyBis/README.md
+
 
 %% first check the Python version
 pyversion
@@ -22,12 +25,47 @@ datasets
 
 %% download dataset
 % get specific dataset
-ds = obi.get_dataset('20101105142049776-6512');
+ds_id = '20101105142049776-6512';
+ds = obi.get_dataset(ds_id);
 % download
 data_dir = '/Users/Henry/Data/Projects/MatlabOpenbis/data';
-ds.download(pyargs('destination', data_dir, 'wait_until_finished', false))
+ds.download(pyargs('destination', data_dir, 'wait_until_finished', false));
+
+%% list files / directories in dataset
+% get list and convert to Matlab cell array
+files = cell(ds.get_file_list);
+for iii = 1:numel(files)
+    iii_file = files{iii};
+    fprintf('%d - %s - isDir: %d\n', iii, string(iii_file{'pathInDataSet'}), iii_file{'isDirectory'})
+end
+
+%% Spaces
+% list spaces
+spaces = obi.get_spaces;
+spaces
+% create a new space
+space_id = 'MATLAB_TEST';
+space = obi.new_space(pyargs('code',  space_id, 'description', 'test space for Matlab access to openBIS'));
+space
+space.save;
+% delete created space afterwards
+% space.delete('just a test');
+
+%% Projects
+% list projects in Space
+space.get_projects
+% create new project
+project_code = 'test_project';
+project_description = 'my awsome project';
+project = space.new_project(pyargs('code', project_code, ...
+    'description' , project_description));
+project = project.save();
+space.get_projects
 
 
 %% clean up
 % logout
 obi.logout()
+
+
+
