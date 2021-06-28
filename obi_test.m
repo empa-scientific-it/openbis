e49acc24982a17cbbcc13ca2e511e13f38e5b9ae
Henry Luetcke <hluetcke@ethz.ch>
2021-06-28 17:55:19 +0200

    add skeletonb for test function

---
+++ obi_test.m
@@ -0,0 +1,32 @@
+function pass = obi_test
+%obi_test Testing function for the Matlab openBIS Toolbox
+%   This function runs a few tests for the Matlab openBIS Toolbox. It
+%   returns true if all the tests pass successfully and fasle otherwise.
+pass = true;
+
+%% 1. Login to openBIS
+
+
+%% 2. Create space for test
+
+
+%% 3. Create project for test
+
+
+%% 4. Create experiment for test
+
+
+%% 5. Create dataset with dummy files
+
+
+%% 6. Download the created dataset
+
+
+%% 7. Tear-down (delete everything, optional)
+
+
+%% 8. Logout
+
+
+end
+
