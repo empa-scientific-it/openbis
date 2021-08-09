5d0a382d945dcf38457ac87a59e497e670bc4987
vermeul <swen@ethz.ch>
2021-08-09 16:45:25 +0200

    fixed tests for person

--- pybis/src/python/tests/test_person.py
+++ pybis/src/python/tests/test_person.py
@@ -8,44 +8,31 @@ from pybis import DataSet
 from pybis import Openbis
 
 
-def test_create_person(space):
-    openbis_instance=space.openbis
-    timestamp = time.strftime('%a_%y%m%d_%H%M%S').upper()
-    person_name = 'test_person_' + timestamp
-    person = openbis_instance.new_person(userId=person_name)
-    person.save()
-    person_exists = openbis_instance.get_person(userId=person_name)
-    assert person_exists is not None
-    
+def test_admin_person_roles(openbis_instance):
+    admin = openbis_instance.get_person(userId="admin")
+    assert admin is not None
+
     # test role assignments
-    roles = person.get_roles()
-    assert len(roles) == 0
+    roles = admin.get_roles()
+    assert len(roles) > 0
 
-    for role in person.get_roles():
-        role.delete('test')
+    admin.assign_role("OBSERVER")
 
-    roles_exist = person.get_roles()
-    assert len(roles_exist) == 0
+    roles = admin.get_roles()
+    observer_role_exists = False
+    for role in roles:
+        if role.role == "OBSERVER":
+            observer_role_exists = True
+    assert observer_role_exists
 
-    person.assign_role('OBSERVER')
-    roles_exist = person.get_roles()
+    admin.assign_role(role="OBSERVER", space="DEFAULT")
+    space_roles_exist = admin.get_roles(space="DEFAULT")
+    assert len(space_roles_exist) == 1
+
+    admin.revoke_role(role="OBSERVER")
+    roles_exist = admin.get_roles()
+    assert len(roles_exist) > 1
+
+    admin.revoke_role(role="OBSERVER", space="DEFAULT")
+    roles_exist = admin.get_roles()
     assert len(roles_exist) == 1
-
-    person.assign_role(role='OBSERVER', space=space)
-    roles_exist = person.get_roles()
-    assert len(roles_exist) == 2
-
-    person.revoke_role(role='OBSERVER')
-    roles_exist = person.get_roles()
-    assert len(roles_exist) == 1
-
-    person.revoke_role(role='OBSERVER', space=space)
-    roles_exist = person.get_roles()
-    assert len(roles_exist) == 0
-
-    #person.delete("test on {}".format(timestamp))
-
-    #with pytest.raises(ValueError):
-    #    person_not_exists = openbis_instance.get_person(userId=person_name)
-    #    assert person_not_exists is None
-
