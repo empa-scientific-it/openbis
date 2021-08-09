3a7752aeea5b5929e89deb71886e6d7efda61534
vermeul <swen@ethz.ch>
2021-08-09 17:16:45 +0200

    black formatting, fixed test (use_cache=False)

--- pybis/src/python/tests/test_tag.py
+++ pybis/src/python/tests/test_tag.py
@@ -6,18 +6,16 @@ import pytest
 import time
 from random import randint
 
-def test_crud_tag(openbis_instance):
-    tag_name = 'test_tag_{}'.format(randint(0,1000)).upper()
-    description='description of tag ' + tag_name
 
-    tag = openbis_instance.new_tag(
-        code=tag_name, 
-        description=description
-    )
+def test_crud_tag(openbis_instance):
+    tag_name = "test_tag_{}".format(randint(0, 1000)).upper()
+    description = "description of tag " + tag_name
+
+    tag = openbis_instance.new_tag(code=tag_name, description=description)
 
     assert tag.code == tag_name
     assert tag.description == description
-    assert tag.permId == ''
+    assert tag.permId == ""
 
     tag.save()
 
@@ -25,30 +23,30 @@ def test_crud_tag(openbis_instance):
 
     tag_exists = openbis_instance.get_tag(tag.permId)
     assert tag_exists is not None
-    
+
     tag_by_code = openbis_instance.get_tag(tag.code)
     assert tag_by_code is not None
     assert tag_by_code.permId == tag_exists.permId
 
-    altered_description = 'altered description of tag ' + tag_name
+    altered_description = "altered description of tag " + tag_name
     tag.description = altered_description
     tag.save()
     assert tag.description == altered_description
 
-    tag.delete('test')
-    
+    tag.delete("test")
+
     with pytest.raises(ValueError):
-        tag_does_not_exists = openbis_instance.get_tag(tag.permId)
-        assert "deleted tag should no longer be present" is None
+        tag_does_not_exists = openbis_instance.get_tag(tag.permId, use_cache=False)
+
 
 def test_get_tags(openbis_instance):
     tags = openbis_instance.get_tags()
     assert tags is not None
-    assert tags.__class__.__name__ == 'Things'
-    assert tags.df.__class__.__name__ == 'DataFrame'
+    assert tags.__class__.__name__ == "Things"
+    assert tags.df.__class__.__name__ == "DataFrame"
 
     if len(tags) > 0:
-        assert tags[0].__class__.__name__ == 'Tag'
+        assert tags[0].__class__.__name__ == "Tag"
 
     if len(tags) > 1:
         tag1 = tags[0]
@@ -56,7 +54,5 @@ def test_get_tags(openbis_instance):
 
         tag_coll = openbis_instance.get_tag([tag1.permId, tag2.permId])
         assert len(tag_coll) == 2
-        assert tag_coll.__class__.__name__ == 'Things'
-        assert tag_coll.df.__class__.__name__ == 'DataFrame'
-
-
+        assert tag_coll.__class__.__name__ == "Things"
+        assert tag_coll.df.__class__.__name__ == "DataFrame"
