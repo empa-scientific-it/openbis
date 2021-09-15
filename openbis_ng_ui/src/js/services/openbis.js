07c9485424871a5161bfed221c04ac88bac25442
pkupczyk <piotr.kupczyk@id.ethz.ch>
2021-09-15 17:00:31 +0200

    SSDM-11664 : NG UI & ELN : common table widget - make sure openbis facade gets initialized only once

--- openbis_ng_ui/src/js/services/openbis.js
+++ openbis_ng_ui/src/js/services/openbis.js
@@ -2,10 +2,16 @@ import dto from '@src/js/services/openbis/dto.js'
 import api from '@src/js/services/openbis/api.js'
 
 class Openbis {
+  constructor() {
+    this.initialized = false
+  }
   async init() {
-    await Promise.all([dto._init(), api._init()])
-    Object.assign(this, dto)
-    Object.assign(this, api)
+    if (!this.initialized) {
+      await Promise.all([dto._init(), api._init()])
+      Object.assign(this, dto)
+      Object.assign(this, api)
+      this.initialized = true
+    }
   }
 }
 
