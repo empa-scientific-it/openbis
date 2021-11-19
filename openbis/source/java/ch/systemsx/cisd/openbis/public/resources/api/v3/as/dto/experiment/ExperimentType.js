34c2151fafd13b7431835b76ed2bf71862b078fe
pkupczyk <piotr.kupczyk@id.ethz.ch>
2021-11-19 13:08:36 +0100

    SSDM-12034 : NG UI : entity validation script display bug (OTRS ID#18458893)

--- openbis/source/java/ch/systemsx/cisd/openbis/public/resources/api/v3/as/dto/experiment/ExperimentType.js
+++ openbis/source/java/ch/systemsx/cisd/openbis/public/resources/api/v3/as/dto/experiment/ExperimentType.js
@@ -2,76 +2,96 @@
  * Class automatically generated with
  * {@link ch.ethz.sis.openbis.generic.shared.api.v3.dto.generators.DtoGenerator}
  */
-define([ "stjs" ], function(stjs) {
-	var ExperimentType = function() {
-	};
-	stjs.extend(ExperimentType, null, [], function(constructor, prototype) {
-		prototype['@type'] = 'as.dto.experiment.ExperimentType';
-		constructor.serialVersionUID = 1;
-		prototype.fetchOptions = null;
-		prototype.permId = null;
-		prototype.code = null;
-		prototype.description = null;
-		prototype.modificationDate = null;
-		prototype.propertyAssignments = null;
-		prototype.validationPlugin = null;
-		prototype.getPropertyAssignments = function() {
-			if (this.getFetchOptions() && this.getFetchOptions().hasPropertyAssignments()) {
-				return this.propertyAssignments;
-			} else {
-				throw new exceptions.NotFetchedException("Property assignments have not been fetched.");
-			}
-		};
-		prototype.setPropertyAssignments = function(propertyAssignments) {
-			this.propertyAssignments = propertyAssignments;
-		};
-		prototype.getValidationPlugin = function() {
-			if (this.getFetchOptions() && this.getFetchOptions().hasValidationPlugin()) {
-				return this.validationPlugin;
-			} else {
-				throw new exceptions.NotFetchedException("Validation plugin have not been fetched.");
-			}
-		};
-		prototype.setValidationPlugin = function(validationPlugin) {
-			this.validationPlugin = validationPlugin;
-		};
-		prototype.getFetchOptions = function() {
-			return this.fetchOptions;
-		};
-		prototype.setFetchOptions = function(fetchOptions) {
-			this.fetchOptions = fetchOptions;
-		};
-		prototype.getPermId = function() {
-			return this.permId;
-		};
-		prototype.setPermId = function(permId) {
-			this.permId = permId;
-		};
-		prototype.getCode = function() {
-			return this.code;
-		};
-		prototype.setCode = function(code) {
-			this.code = code;
-		};
-		prototype.getDescription = function() {
-			return this.description;
-		};
-		prototype.setDescription = function(description) {
-			this.description = description;
-		};
-		prototype.getModificationDate = function() {
-			return this.modificationDate;
-		};
-		prototype.setModificationDate = function(modificationDate) {
-			this.modificationDate = modificationDate;
-		};
-		prototype.toString = function() {
-			return this.getCode();
-		};
-	}, {
-		fetchOptions : "ExperimentTypeFetchOptions",
-		permId : "EntityTypePermId",
-		modificationDate : "Date"
-	});
-	return ExperimentType;
+define(['stjs'], function (stjs) {
+  var ExperimentType = function () {}
+  stjs.extend(
+    ExperimentType,
+    null,
+    [],
+    function (constructor, prototype) {
+      prototype['@type'] = 'as.dto.experiment.ExperimentType'
+      constructor.serialVersionUID = 1
+      prototype.fetchOptions = null
+      prototype.permId = null
+      prototype.code = null
+      prototype.description = null
+      prototype.modificationDate = null
+      prototype.propertyAssignments = null
+      prototype.validationPlugin = null
+      prototype.getPropertyAssignments = function () {
+        if (
+          this.getFetchOptions() &&
+          this.getFetchOptions().hasPropertyAssignments()
+        ) {
+          return this.propertyAssignments
+        } else {
+          throw new exceptions.NotFetchedException(
+            'Property assignments have not been fetched.'
+          )
+        }
+      }
+      prototype.setPropertyAssignments = function (propertyAssignments) {
+        this.propertyAssignments = propertyAssignments
+      }
+      prototype.getValidationPlugin = function () {
+        if (
+          this.getFetchOptions() &&
+          this.getFetchOptions().hasValidationPlugin()
+        ) {
+          return this.validationPlugin
+        } else {
+          throw new exceptions.NotFetchedException(
+            'Validation plugin have not been fetched.'
+          )
+        }
+      }
+      prototype.setValidationPlugin = function (validationPlugin) {
+        this.validationPlugin = validationPlugin
+      }
+      prototype.getFetchOptions = function () {
+        return this.fetchOptions
+      }
+      prototype.setFetchOptions = function (fetchOptions) {
+        this.fetchOptions = fetchOptions
+      }
+      prototype.getPermId = function () {
+        return this.permId
+      }
+      prototype.setPermId = function (permId) {
+        this.permId = permId
+      }
+      prototype.getCode = function () {
+        return this.code
+      }
+      prototype.setCode = function (code) {
+        this.code = code
+      }
+      prototype.getDescription = function () {
+        return this.description
+      }
+      prototype.setDescription = function (description) {
+        this.description = description
+      }
+      prototype.getModificationDate = function () {
+        return this.modificationDate
+      }
+      prototype.setModificationDate = function (modificationDate) {
+        this.modificationDate = modificationDate
+      }
+      prototype.toString = function () {
+        return this.getCode()
+      }
+    },
+    {
+      fetchOptions: 'ExperimentTypeFetchOptions',
+      permId: 'EntityTypePermId',
+      modificationDate: 'Date',
+      propertyAssignments: {
+        name: 'List',
+        arguments: ['PropertyAssignment']
+      },
+      validationPlugin: 'Plugin'
+    }
+  )
+  return ExperimentType
 })
