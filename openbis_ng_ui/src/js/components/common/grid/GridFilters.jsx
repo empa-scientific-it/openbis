46ef4581a5de43abbaa17c7ac6b90869f827ab5a
pkupczyk <piotr.kupczyk@id.ethz.ch>
2022-01-19 14:14:11 +0100

    SSDM-12155 : Table Widget : Improve table usability - clean up grid filters section

--- openbis_ng_ui/src/js/components/common/grid/GridFilters.jsx
+++ openbis_ng_ui/src/js/components/common/grid/GridFilters.jsx
@@ -5,153 +5,104 @@ import GridFilter from '@src/js/components/common/grid/GridFilter.jsx'
 import GridFilterOptions from '@src/js/components/common/grid/GridFilterOptions.js'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
-import SelectField from '@src/js/components/common/form/SelectField.jsx'
-import TextField from '@src/js/components/common/form/TextField.jsx'
-import messages from '@src/js/common/messages.js'
 import logger from '@src/js/common/logger.js'
 
 const styles = theme => ({
   multiselectCell: {
-    padding: 0,
+    padding: 0
+  },
+  columnFilterCell: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
     paddingLeft: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-    minWidth: 50
+    paddingRight: 0,
+    '&:last-child': {
+      paddingRight: theme.spacing(2)
+    }
   },
-  noFilters: {
-    padding: 0,
-    paddingTop: theme.spacing(1)
-  },
-  filterTypeCell: {
-    borderBottom: 0,
-    paddingBottom: 0,
-    paddingTop: theme.spacing(1)
+  globalFilterCell: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
   }
 })
 
 class GridFilters extends React.PureComponent {
-  constructor(props) {
-    super(props)
-    this.handleFilterModeChange = this.handleFilterModeChange.bind(this)
-  }
-
-  handleFilterModeChange(event) {
-    const { onFilterModeChange } = this.props
-    if (onFilterModeChange) {
-      onFilterModeChange(event.target.value)
-    }
-  }
-
   render() {
     logger.log(logger.DEBUG, 'GridFilters.render')
 
     const { columns, filterModes, filterMode } = this.props
 
+    if (columns.length === 0) {
+      return null
+    }
+
     if (filterModes && !filterModes.includes(filterMode)) {
-      return (
-        <TableRow>
-          {this.renderMultiselectCell()}
-          {this.renderNoFiltersCell()}
-        </TableRow>
-      )
+      return null
     } else if (filterMode === GridFilterOptions.GLOBAL_FILTER) {
-      return (
-        <React.Fragment>
-          <TableRow>{this.renderGlobalFilterCell()}</TableRow>
-        </React.Fragment>
-      )
+      return <TableRow>{this.renderGlobalFilterCell()}</TableRow>
     } else if (filterMode === GridFilterOptions.COLUMN_FILTERS) {
-      return (
-        <React.Fragment>
+      const someFilterable = columns.some(column => column.filterable)
+      if (someFilterable) {
+        return (
           <TableRow>
             {this.renderMultiselectCell()}
-            {columns.map(column => this.renderFilterCell(column))}
+            {this.renderColumnFiltersCells()}
           </TableRow>
-        </React.Fragment>
-      )
+        )
+      } else {
+        return null
+      }
     } else {
       throw new Error('Unsupported filter mode: ' + filterMode)
     }
   }
 
-  renderNoFiltersCell() {
-    const { columns, classes } = this.props
+  renderGlobalFilterCell() {
+    const {
+      columns,
+      multiselectable,
+      globalFilter,
+      onGlobalFilterChange,
+      classes
+    } = this.props
+
     return (
       <TableCell
-        colSpan={columns.length}
-        classes={{ root: classes.noFilters }}
-      ></TableCell>
+        colSpan={columns.length + (multiselectable ? 1 : 0)}
+        classes={{ root: classes.globalFilterCell }}
+      >
+        <GridGlobalFilter
+          globalFilter={globalFilter}
+          onGlobalFilterChange={onGlobalFilterChange}
+        />
+      </TableCell>
     )
   }
 
-  renderGlobalFilterCell() {
-    const { columns, globalFilter, onGlobalFilterChange } = this.props
+  renderColumnFiltersCells() {
+    const { columns, filters, onFilterChange, classes } = this.props
 
-    return (
-      <GridGlobalFilter
-        columns={columns}
-        globalFilter={globalFilter}
-        onGlobalFilterChange={onGlobalFilterChange}
-      />
-    )
-  }
-
-  renderFilterCell(column) {
-    const { filters, onFilterChange } = this.props
-
-    return (
-      <GridFilter
-        key={column.name}
-        column={column}
-        filter={filters[column.name]}
-        onFilterChange={onFilterChange}
-      />
-    )
+    return columns.map(column => (
+      <TableCell key={column.name} classes={{ root: classes.columnFilterCell }}>
+        <GridFilter
+          column={column}
+          filter={filters[column.name]}
+          onFilterChange={onFilterChange}
+        />
+      </TableCell>
+    ))
   }
 
   renderMultiselectCell() {
     const { columns, multiselectable, classes } = this.props
 
     if (columns.length > 0 && multiselectable) {
-      return (
-        <TableCell classes={{ root: classes.multiselectCell }}>
-          <TextField label='Filter' variant='standard' />
-        </TableCell>
-      )
+      return <TableCell classes={{ root: classes.multiselectCell }}></TableCell>
     } else {
       return null
     }
-  }
-
-  renderFilterType() {
-    const { filterMode, columns, classes } = this.props
-
-    return (
-      <TableRow>
-        <TableCell
-          colSpan={columns.length + 1}
-          classes={{ root: classes.filterTypeCell }}
-        >
-          <SelectField
-            name='filters'
-            label='Filter Type'
-            options={[
-              {
-                label: messages.get(messages.GLOBAL_FILTER),
-                value: GridFilterOptions.GLOBAL_FILTER
-              },
-              {
-                label: messages.get(messages.COLUMN_FILTERS),
-                value: GridFilterOptions.COLUMN_FILTERS
-              }
-            ]}
-            value={filterMode}
-            variant='standard'
-            fullWidth={false}
-            onChange={this.handleFilterModeChange}
-          />
-        </TableCell>
-      </TableRow>
-    )
   }
 }
 
