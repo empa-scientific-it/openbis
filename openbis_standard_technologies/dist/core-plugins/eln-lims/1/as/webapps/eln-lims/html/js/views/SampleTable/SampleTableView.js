b0b67b490090c36123d02c4aee8ebfa9c144c928
felmer <franz-josef.elmer@id.ethz.ch>
2022-02-08 09:03:36 +0100

    SSDM-8701: Change menu item text from 'NEW Batch ...' to 'XLS Batch ...'

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/SampleTable/SampleTableView.js
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/SampleTable/SampleTableView.js
@@ -138,15 +138,17 @@ function SampleTableView(sampleTableController, sampleTableModel) {
 		}
 		
 		
-        var title = "NEW Batch Register " + ELNDictionary.Samples;
-        var $xslBatchRegisterOption = $("<li>", { 'role' : 'presentation' }).append($("<a>", {'title' : title, 'id' : 'xsl-register-' + ELNDictionary.Sample.toLowerCase() + '-btn'}).append(title));
+        var label = "XLS Batch Register " + ELNDictionary.Samples;
+        var id = 'xsl-register-' + ELNDictionary.Sample.toLowerCase() + '-btn';
+        var $xslBatchRegisterOption = $("<li>", { 'role' : 'presentation' }).append($("<a>", {'title' : label, 'id' : id}).append(label));
         $xslBatchRegisterOption.click(function() {
             _this._sampleTableController.registerSamples(_this._sampleTableModel.experimentIdentifier);
         });
         $list.append($xslBatchRegisterOption);
 
-        var title = "NEW Batch Update " + ELNDictionary.Samples;
-        var $xslBatchUpdateOption = $("<li>", { 'role' : 'presentation' }).append($("<a>", {'title' : title, 'id' : 'xsl-update-' + ELNDictionary.Sample.toLowerCase() + '-btn'}).append(title));
+        var label = "XLS Batch Update " + ELNDictionary.Samples;
+        var id = 'xsl-update-' + ELNDictionary.Sample.toLowerCase() + '-btn';
+        var $xslBatchUpdateOption = $("<li>", { 'role' : 'presentation' }).append($("<a>", {'title' : label, 'id' : id}).append(label));
         $xslBatchUpdateOption.click(function() {
             _this._sampleTableController.updateSamples(_this._sampleTableModel.experimentIdentifier);
         });
