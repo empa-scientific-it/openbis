2ae0e07e3db53b9ee31caceb3e83e64014100027
Viktor Kovtun <viktor.kovtun@id.ethz.ch>
2022-02-16 18:15:56 +0100

    SSDM-12286 Added vocabularies.

--- openbis/source/java/ch/ethz/sis/openbis/generic/server/xls/export/helper/IXLSExportHelper.java
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/xls/export/helper/IXLSExportHelper.java
@@ -3,12 +3,11 @@ package ch.ethz.sis.openbis.generic.server.xls.export.helper;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import ch.ethz.sis.openbis.generic.asapi.v3.IApplicationServerApi;
-import ch.ethz.sis.openbis.generic.server.xls.export.ExportablePermId;
 
 public interface IXLSExportHelper
 {
 
-    int add(final IApplicationServerApi api, final String sessionToken, final Workbook wb, final ExportablePermId exportablePermId,
-            final int rowNumber);
+    int add(final IApplicationServerApi api, final String sessionToken, final Workbook wb,
+            final String permId, final int rowNumber);
 
 }
