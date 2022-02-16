2a0795675389c568851e2619f642430ae02d59be
Viktor Kovtun <viktor.kovtun@id.ethz.ch>
2022-02-16 18:15:56 +0100

    SSDM-12286 Added vocabularies.
    (cherry picked from commit 2ae0e07e3db53b9ee31caceb3e83e64014100027)

--- openbis/source/java/ch/ethz/sis/openbis/generic/server/xls/export/XLSExport.java
+++ openbis/source/java/ch/ethz/sis/openbis/generic/server/xls/export/XLSExport.java
@@ -8,6 +8,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,12 +22,15 @@ import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.id.PropertyTypePermId;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.vocabulary.id.VocabularyPermId;
 import ch.ethz.sis.openbis.generic.server.xls.export.helper.IXLSExportHelper;
 import ch.ethz.sis.openbis.generic.server.xls.export.helper.XLSSampleTypeExportHelper;
+import ch.ethz.sis.openbis.generic.server.xls.export.helper.XLSVocabularyExportHelper;
 import ch.systemsx.cisd.openbis.generic.shared.OpenBisServiceV3Factory;
 
 public class XLSExport
 {
 
     private final IXLSExportHelper sampleTypeExportHelper = new XLSSampleTypeExportHelper();
+
+    private final IXLSExportHelper vocabularyExportHelper = new XLSVocabularyExportHelper();
 
     Workbook prepareWorkbook(final IApplicationServerApi api, final String sessionToken,
             Collection<ExportablePermId> exportablePermIds, final boolean exportReferred) throws IOException
@@ -51,18 +55,20 @@ public class XLSExport
             switch (exportablePermId.getExportableKind())
             {
                 case SAMPLE_TYPE:
-                    rowNumber = sampleTypeExportHelper.add(api, sessionToken, wb, exportablePermId, rowNumber);
+                    rowNumber = sampleTypeExportHelper.add(api, sessionToken, wb,
+                            exportablePermId.getPermId().getPermId(), rowNumber);
                     break;
                 case EXPERIMENT_TYPE:
                     break;
                 case DATASET_TYPE:
                     break;
                 case VOCABULARY:
+                    rowNumber = vocabularyExportHelper.add(api, sessionToken, wb,
+                            exportablePermId.getPermId().getPermId(), rowNumber);
                     break;
                 case PROPERTY_TYPE:
                     break;
             }
-            rowNumber++;
         }
 
         return wb;
@@ -131,22 +137,39 @@ public class XLSExport
     public static void main(String[] args) throws IOException
     {
         final XLSExport xlsExport = new XLSExport();
-//        final IApplicationServerApi applicationServerApi = new ApplicationServerApi();
 
         final OpenBisServiceV3Factory openBisServiceV3Factory =
                 new OpenBisServiceV3Factory("http://localhost:8888/");
         final IApplicationServerApi applicationServerApi = openBisServiceV3Factory.createService();
         final String sessionToken = applicationServerApi.login("admin", "changeit");
 
-        final Collection<ExportablePermId> permIds = Stream.of("UNKNOWN", "STORAGE", "ENTRY", "PUBLICATION", "STORAGE_POSITION",
+        final Collection<ExportablePermId> vocabularies = Stream.of("STORAGE_FORMAT", "DEFAULT_COLLECTION_VIEWS",
+                        "SUPPLIER.PREFERRED_ORDER_METHOD", "STORAGE_POSITION.STORAGE_BOX_SIZE",
+                        "ORDER.ORDER_STATUS", "SUPPLIER.LANGUAGE", "WELL.COLOR_ENCODED_ANNOTATIONS",
+                        "PRODUCT.CURRENCY", "STORAGE.STORAGE_VALIDATION_LEVEL", "ANTIBODY.DETECTION",
+                        "YEAST.BACKGROUND_SPECIFIC_MARKERS", "MEDIA.ORGANISM", "RNA.RNA_BACKBONE", "OLIGO.DIRECTION",
+                        "ANTIBODY.CLONALITY", "YEAST.GENETIC_BACKGROUND", "YEAST.MATING_TYPE", "PLASMID.BACKBONE",
+                        "ORIGIN", "ANNOTATION.PLASMID_RELATIONSHIP", "CELL_LINE.YES_NO_CHOICE", "RNA.STRAND",
+                        "CELL_LINE.CELL_MEDIUM", "ANTIBODY.HOST", "RNA.RNA_TYPE", "CELL_LINE.SPECIES", "STERILIZATION",
+                        "LIFE_SCIENCES_TYPES.VERSION", "PLASMID.MARKER", "YEAST.COMMON_MARKERS",
+                        "PCR_PROTOCOL.TEMPLATE", "PLASMID.BACTERIAL_ANTIBIOTIC_RESISTANCE", "STORAGE_CONDITIONS",
+                        "YEAST.ENDOGENOUS_PLASMID", "CHECK", "CELL_LINE.CELL_TYPE",
+                        "WESTERN_BLOTTING_PROTOCOL.MEMBRANE")
+                .map(code -> new ExportablePermId(ExportableKind.VOCABULARY, new VocabularyPermId(code)))
+                .collect(Collectors.toList());
+
+        final Collection<ExportablePermId> sampleTypes = Stream.of("UNKNOWN", "STORAGE", "ENTRY", "PUBLICATION", "STORAGE_POSITION",
                         "SUPPLIER", "ORDER", "REQUEST", "PRODUCT", "GENERAL_ELN_SETTINGS", "GENERAL_PROTOCOL",
                         "EXPERIMENTAL_STEP", "BACTERIA", "PCR_PROTOCOL", "RNA", "PLASMID", "OLIGO", "ANTIBODY",
                         "WESTERN_BLOTTING_PROTOCOL", "SOLUTION_BUFFER", "YEAST", "PLANT_SPECIES", "FLY", "CELL_LINE",
                         "ENZYME", "MEDIA", "CHEMICAL")
                 .map(code -> new ExportablePermId(ExportableKind.SAMPLE_TYPE, new EntityTypePermId(code, SAMPLE)))
                 .collect(Collectors.toList());
-        final ByteArrayOutputStream os = xlsExport.export(applicationServerApi, sessionToken,
-                permIds, false);
+
+        final Collection<ExportablePermId> exportablePermIds = new ArrayList<>();
+        exportablePermIds.addAll(vocabularies);
+        exportablePermIds.addAll(sampleTypes);
+        final ByteArrayOutputStream os = xlsExport.export(applicationServerApi, sessionToken, exportablePermIds, false);
 
         try (final OutputStream fileOutputStream = new FileOutputStream("test.xlsx"))
         {
