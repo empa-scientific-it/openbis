4784356685dbbd2a910953d250d81417de1f5f5d
pkupczyk <piotr.kupczyk@id.ethz.ch>
2022-03-18 14:26:16 +0100

    Remove no longer needed Redux and Redux-saga files

--- openbis_ng_ui/src/js/store/sagas/sagas.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects'
-import app from '@src/js/store/sagas/app.js'
-import api from '@src/js/store/sagas/api.js'
-import page from '@src/js/store/sagas/page.js'
-
-export default function* root() {
-  yield all([api(), app(), page()])
-}
