7a5e419761bcb5214b2301dcfd496d0062fcdcfb
pkupczyk <piotr.kupczyk@id.ethz.ch>
2022-03-18 15:16:45 +0100

    Add a parameter to control log level of diff

--- openbis_ng_ui/src/js/common/diff.js
+++ openbis_ng_ui/src/js/common/diff.js
@@ -1,23 +1,23 @@
 import _ from 'lodash'
 import logger from '@src/js/common/logger.js'
 
-export default function diff(beforeState, afterState, path) {
+export default function diff(level, beforeState, afterState, path) {
   let unmodifiedNewObjects = false
 
   if (_.isObject(beforeState) && _.isObject(afterState)) {
     if (beforeState === afterState) {
-      logger.log(logger.TRACE, 'OK - ' + path + ' - same object')
+      logger.log(level, 'OK - ' + path + ' - same object')
     } else {
       if (_.isEqual(beforeState, afterState)) {
         logger.log(
-          logger.DEBUG,
+          level,
           'ERROR - ' + path + ' - new object without changes',
           afterState
         )
         unmodifiedNewObjects = true
       } else {
         logger.log(
-          logger.DEBUG,
+          level,
           'OK* - ' + path + ' - new object with changes',
           beforeState,
           afterState
@@ -29,15 +29,16 @@ export default function diff(beforeState, afterState, path) {
         let beforeProp = beforeState ? beforeState[prop] : undefined
         let afterProp = afterState ? afterState[prop] : undefined
         unmodifiedNewObjects =
-          unmodifiedNewObjects | diff(beforeProp, afterProp, path + '/' + prop)
+          unmodifiedNewObjects |
+          diff(level, beforeProp, afterProp, path + '/' + prop)
       })
     }
   } else {
     if (_.isEqual(beforeState, afterState)) {
-      logger.log(logger.TRACE, 'OK - ' + path + ' - same value', afterState)
+      logger.log(level, 'OK - ' + path + ' - same value', afterState)
     } else {
       logger.log(
-        logger.DEBUG,
+        level,
         'OK* - ' + path + ' - changed value',
         beforeState,
         afterState
