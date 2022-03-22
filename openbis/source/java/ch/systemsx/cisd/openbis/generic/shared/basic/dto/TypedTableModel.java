d5858efa0a083aa02fef2806cb71ae89218770ee
felmer <franz-josef.elmer@id.ethz.ch>
2022-03-22 08:47:10 +0100

    SSDM-12359: remove import statement which is only used in JavaDoc comment. This lead to a failure when JavaDoc is created.

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/TypedTableModel.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/basic/dto/TypedTableModel.java
@@ -19,8 +19,6 @@ package ch.systemsx.cisd.openbis.generic.shared.basic.dto;
 import java.io.Serializable;
 import java.util.List;
 
-import ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.TypedTableGrid;
-
 /**
  * Table model for {@link TypedTableGrid} classes. It contains column meta-data as a list of {@link TableModelColumnHeader} instances and the row data
  * as a list of {@link TableModelRowWithObject} instances for row objects of type <code>T</code>.
