ab418204f8a7607893cba5f203718b83fbeeb041
felmer <franz-josef.elmer@id.ethz.ch>
2022-04-20 08:40:17 +0200

    SSDM-11273: disable fetching Javascript console logs because there is still an exception thrown

--- js-test/source/java/ch/systemsx/cisd/openbis/jstest/suite/common/JsTestCommonSelenium.java
+++ js-test/source/java/ch/systemsx/cisd/openbis/jstest/suite/common/JsTestCommonSelenium.java
@@ -139,12 +139,12 @@ public class JsTestCommonSelenium extends SeleniumTest
                             + "/TEST-" + method + ".xml");
             FileUtilities.writeToFile(report, junitReport);
             System.out.println("report for " + method + ":\n" + junitReport);
-            saveConsoleLog(method);
+//            saveConsoleLog(method);
 
             // Assert.assertEquals(0, webapp.getFailedCount());
         } catch (Throwable t)
         {
-            saveConsoleLog(method);
+//            saveConsoleLog(method);
             throw new AssertionError("Test runner throws exception: " + t, t);
         } finally
         {
