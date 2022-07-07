b1a83a4270bdca35627a246d81f275d1ada60503
juanf <juanf@ethz.ch>
2022-07-07 14:25:42 +0200

    SSDM-12655 : closing logins, ongoing work

--- openbis_standard_technologies/dist/core-plugins/eln-lims-life-sciences/1/as/initialize-master-data.py
+++ openbis_standard_technologies/dist/core-plugins/eln-lims-life-sciences/1/as/initialize-master-data.py
@@ -28,6 +28,7 @@ props = CustomASServiceExecutionOptions().withParameter('xls', helper.listXlsByt
     .withParameter('xls_name', 'ELN-LIMS-LIFE-SCIENCES').withParameter('update_mode', 'UPDATE_IF_EXISTS') \
     .withParameter('scripts', helper.getAllScripts())
 result = api.executeCustomASService(sessionToken, CustomASServiceCode("xls-import-api"), props)
+api.logout(sessionToken)
 print("======================== master-data xls ingestion result ========================")
 print(result)
 print("======================== master-data xls ingestion result ========================")
