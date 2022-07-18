517d33d8b0ab807fd292a58b11b74e3f77274e7c
Mariia Pukhliakova <mariia.pukhliakova@id.ethz.ch>
2022-07-18 13:14:43 +0200

    SSDM-12625 use css for ActiveUserReportForm.jsx

--- openbis_ng_ui/src/js/components/tools/form/activeUserReport/ActiveUserReportForm.jsx
+++ openbis_ng_ui/src/js/components/tools/form/activeUserReport/ActiveUserReportForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
 import autoBind from 'auto-bind'
 import Link from '@material-ui/core/Link'
 import logger from '@src/js/common/logger.js'
@@ -10,6 +11,14 @@ import Container from '@src/js/components/common/form/Container.jsx'
 import ComponentContext from '@src/js/components/common/ComponentContext.js'
 import ActiveUserReportFacade from '@src/js/components/tools/form/activeUserReport/ActiveUserReportFacade.js'
 import ActiveUserReportController from '@src/js/components/tools/form/activeUserReport/ActiveUserReportController.js'
+
+const styles = theme => ({
+  message: {
+    display: 'flex',
+    paddingBottom: theme.spacing(1),
+    paddingTop: theme.spacing(1)
+  }
+})
 
 class ActiveUserReportForm extends React.PureComponent {
   constructor(props) {
@@ -29,22 +38,21 @@ class ActiveUserReportForm extends React.PureComponent {
   render() {
     logger.log(logger.DEBUG, 'ActiveUserReportForm.render')
 
-    const { activeUsersCount } = this.props
+    const { activeUsersCount, classes } = this.props
     const { result } = this.state
 
     return (
       <Container>
         <Header>{messages.get(messages.ACTIVE_USERS_REPORT)}</Header>
-        <span>
-          {messages.get(messages.ACTIVE_USERS_REPORT_DIALOG, activeUsersCount)}
-          {" "}
-          <Link href="mailto:cisd.helpdesk@bsse.ethz.ch" target="_blank">{messages.get(messages.HELPDESK)}</Link>
-          {". "}
-          <br/><br/>
-        </span>
-        <Button name='sendReport' label={messages.get(messages.SEND_REPORT)} onClick={this.controller.sendReport}/>
-        <br/>
-        {this.renderResult()}
+        <div className={classes.message}>
+          <Message>{messages.get(messages.ACTIVE_USERS_REPORT_DIALOG, activeUsersCount)}</Message>
+          <div>&nbsp;</div>
+          <Link href="mailto:cisd.helpdesk@bsse.ethz.ch" target="_blank">{messages.get(messages.HELPDESK)}</Link>{". "}
+        </div>
+        <Button name='sendReport' label={messages.get(messages.SEND_REPORT)} onClick={this.controller.sendReport} styles={{ root: classes.button }}/>
+        <div className={classes.message}>
+          {this.renderResult()}
+        </div>
       </Container>
     )
   }
@@ -57,13 +65,11 @@ class ActiveUserReportForm extends React.PureComponent {
     }
 
     return (
-      <Container>
-        <Message type={result.success ? 'success' : 'error'}>
-          {result.success ? messages.get(messages.ACTIVE_USERS_REPORT_EMAIL_SENT_CONFIRMATION) : result.output}
-        </Message>
-      </Container>
+      <Message type={result.success ? 'success' : 'error'}>
+        {result.success ? messages.get(messages.ACTIVE_USERS_REPORT_EMAIL_SENT_CONFIRMATION) : result.output}
+      </Message>
     )
   }
 }
 
-export default ActiveUserReportForm
+export default withStyles(styles)(ActiveUserReportForm)
