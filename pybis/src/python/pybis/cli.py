dd010343a80a5531ba6fdb2978c77c184f37e77f
vermeul <swen@ethz.ch>
2022-08-23 14:30:22 +0200

    implement pat (ongoing)

--- pybis/src/python/pybis/cli.py
+++ pybis/src/python/pybis/cli.py
@@ -2,13 +2,15 @@ from lib2to3.pgen2.token import OP
 import click
 from tabulate import tabulate
 from . import pybis
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import sys
 
 
-def common_options(func):
+def login_options(func):
     options = [
-        click.argument("hostname", required=False),
+        click.argument("hostname"),
         click.option("-u", "--username", help="Username"),
-        click.option("-t", "--token", help="Session Token or Personal Access Token"),
         click.option(
             "--verify-certificate",
             is_flag=True,
@@ -22,42 +24,87 @@ def common_options(func):
     return func
 
 
+def login(hostname, **kwargs):
+
+    openbis = pybis.Openbis(
+        url=hostname,
+        verify_certificates=kwargs.get("verify_certificate", True),
+    )
+    if openbis.is_session_active():
+        return openbis
+
+    if not kwargs.get("username"):
+        kwargs["username"] = click.prompt("Username")
+    if not kwargs.get("password"):
+        kwargs["password"] = click.prompt("Password", hide_input=True)
+    try:
+        openbis.login(
+            username=kwargs["username"],
+            password=kwargs["password"],
+            save_token=True,
+        )
+        return openbis
+    except ValueError as exc:
+        click.echo(f"Failed to login to {hostname}")
+
+
 @click.group()
-@click.pass_context
-def cli(ctx):
+def cli():
     """pybis - command line access to openBIS"""
-    pass
 
 
-@cli.group()
-def get():
-    """get openBIS entities"""
+@cli.command("local", context_settings=dict(ignore_unknown_options=True))
+@click.argument("hostname", required=False)
+@click.argument("token", required=False, type=click.UNPROCESSED)
+@click.option("--info", is_flag=True, help="get more detailed information")
+def get_set_hostname(hostname, token, info):
+    """show or set hostname and token that is used in the current working directory."""
+    if hostname:
+        if token and token.startswith("-"):
+            token = "$pat" + token
+        pybis.set_local_config(hostname=hostname, token=token)
+    else:
+        # get hostname and token stored in .pybis.json
+        config = pybis.get_local_config()
+        if info:
+            o = pybis.Openbis(url=config.get("hostname", ""))
+            session_info = o.get_session_info(token=config.get("token"))
+            print(session_info)
+        click.echo(
+            tabulate(
+                [[config.get("hostname", ""), config.get("token", "")]],
+                headers=["openBIS hostname", "token"],
+            )
+        )
 
 
-@get.command("tokens")
-@common_options
+@cli.command("tokens")
 @click.pass_obj
 def get_tokens(ctx, **kwargs):
-    """get openBIS Session Tokens"""
+    """list stored openBIS Session Tokens"""
     tokens = pybis.get_saved_tokens()
     token_list = [[key, *tokens[key]] for key in tokens]
-    print(tabulate(token_list, headers=["openBIS hostname", "token"]))
+    click.echo(tabulate(token_list, headers=["openBIS hostname", "session token"]))
 
 
-@get.command("pats")
-@click.option("--remote", "-r", is_flag=True, default=False, help="")
-@common_options
+@cli.command("pats")
+@click.argument("hostname", required=False)
 @click.argument("session-name", required=False)
 @click.pass_obj
-def get_pats(ctx, hostname, session_name=None, **kwargs):
-    """get openBIS Personal Access Tokens (PAT)"""
+def get_pats(ctx, hostname, session_name=None):
+    """list stored openBIS Personal Access Tokens (PAT)"""
     tokens = pybis.get_saved_pats(hostname=hostname, sessionName=session_name)
-    headers = ["permId", "hostname", "sessionName", "validToDate"]
+    headers = ["hostname", "permId", "sessionName", "validToDate"]
     token_list = [[token[key] for key in headers] for token in tokens]
-    print(
+    click.echo(
         tabulate(
             token_list,
-            headers=["token", "openBIS hostname", "sessionName", "valid until"],
+            headers=[
+                "openBIS hostname",
+                "personal access token",
+                "sessionName",
+                "valid until",
+            ],
         )
     )
 
@@ -66,11 +113,10 @@ def get_pats(ctx, hostname, session_name=None, **kwargs):
 @click.pass_obj
 def new(ctx):
     """new openBIS entity"""
-    click.echo("get()")
+    pass
 
 
 @new.command("token")
-@common_options
 @click.pass_obj
 def new_token(ctx, **kwargs):
     """create new openBIS Session Token"""
@@ -78,46 +124,27 @@ def new_token(ctx, **kwargs):
 
 
 @new.command("pat")
-@common_options
+@login_options
+@click.argument("session-name")
+@click.option("--validity-days", help="Number of days the token is valid")
+@click.option("--validity-weeks", help="Number of weeks the token is valid")
+@click.option("--validity-months", help="Number of months the token is valid")
 @click.pass_obj
-def new_pat(ctx, **kwargs):
+def new_pat(ctx, hostname, session_name, **kwargs):
     """create new openBIS Personal Access Token"""
-    click.echo("new_pat()")
-
-
-@cli.group()
-def up():
-    """update openBIS entities"""
-    click.echo("up")
-
-
-@up.command("token")
-@common_options
-@click.pass_obj
-def up_token(ctx, **kwargs):
-    """update openBIS session token"""
-    click.echo("up token")
-
-
-@up.command("pat")
-@click.pass_obj
-def up_token(ctx):
-    """update openBIS Personal Access Token"""
-    click.echo("up token")
-
-
-@cli.group("del")
-@common_options
-@click.pass_obj
-def delete(ctx, **kwargs):
-    """delete openBIS entities"""
-    click.echo("del")
-
-
-@delete.command("token")
-def del_token():
-    """delete openBIS token"""
-    click.echo("delete token")
+    validTo = datetime.now()
+    if kwargs.get("validity_months"):
+        validTo += relativedelta(months=int(kwargs.get("validity_months")))
+    elif kwargs.get("validity_weeks"):
+        validTo += relativedelta(weeks=int(kwargs.get("validity_weeks")))
+    elif kwargs.get("validity_days"):
+        validTo += relativedelta(days=int(kwargs.get("validity_days")))
+    else:
+        validTo += relativedelta(years=1)
+    o = login(hostname, **kwargs)
+    new_pat = o.create_personal_access_token(sessionName=session_name, validTo=validTo)
+    click.echo(new_pat)
+    o.get_personal_access_tokens(save_to_disk=True)
 
 
 if __name__ == "__main__":
