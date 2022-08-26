07bcf1bfb71cd7fb5c17b760a26a80875701b5bc
pkupczyk <piotr.kupczyk@id.ethz.ch>
2022-08-26 10:03:55 +0200

    SSDM-11550 : Personal Access Tokens (PAT) - change defaults for the max validity period to 30 days and the warning period to 5 days

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/pat/PersonalAccessTokenConstants.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/pat/PersonalAccessTokenConstants.java
@@ -11,11 +11,11 @@ public class PersonalAccessTokenConstants
 
     public static final String PERSONAL_ACCESS_TOKENS_MAX_VALIDITY_PERIOD = "personal-access-tokens-max-validity-period";
 
-    public static final long PERSONAL_ACCESS_TOKENS_MAX_VALIDITY_PERIOD_DEFAULT = 90 * DAY_IN_SECONDS;
+    public static final long PERSONAL_ACCESS_TOKENS_MAX_VALIDITY_PERIOD_DEFAULT = 30 * DAY_IN_SECONDS;
 
     public static final String PERSONAL_ACCESS_TOKENS_VALIDITY_WARNING_PERIOD = "personal-access-tokens-validity-warning-period";
 
-    public static final long PERSONAL_ACCESS_TOKENS_VALIDITY_WARNING_PERIOD_DEFAULT = 10 * DAY_IN_SECONDS;
+    public static final long PERSONAL_ACCESS_TOKENS_VALIDITY_WARNING_PERIOD_DEFAULT = 5 * DAY_IN_SECONDS;
 
     public static final String PERSONAL_ACCESS_TOKENS_FILE_PATH = "personal-access-tokens-file-path";
 
