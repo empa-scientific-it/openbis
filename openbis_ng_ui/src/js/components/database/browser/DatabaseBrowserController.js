171ad014e56245bf95017ea43d477f8e30ae4ef4
pkupczyk <piotr.kupczyk@id.ethz.ch>
2022-08-31 15:42:46 +0200

    SSDM-12451 : Navigation - generate a larger number of nodes to stress performance

--- openbis_ng_ui/src/js/components/database/browser/DatabaseBrowserController.js
+++ openbis_ng_ui/src/js/components/database/browser/DatabaseBrowserController.js
@@ -1,24 +1,43 @@
-import pages from '@src/js/common/consts/pages.js'
 import BrowserController from '@src/js/components/common/browser2/BrowserController.js'
 
 export default class UserBrowserController extends BrowserController {
-  doGetPage() {
-    return pages.DATABASE
-  }
-
   async doLoadNodes() {
+    var children2 = []
+
+    for (let i = 0; i < 1000; i++) {
+      children2.push({
+        id: 'child2.' + i,
+        text: 'Child 2.' + i,
+        object: { type: 'child', id: '2.' + i },
+        canMatchFilter: true
+      })
+    }
+
     let nodes = [
       {
         id: 'folder1',
         text: 'Folder 1',
         object: { type: 'folder', id: '1' },
-        children: []
+        children: [
+          {
+            id: 'child1.1',
+            text: 'Child 1.1',
+            object: { type: 'child', id: '1.1' },
+            canMatchFilter: true
+          },
+          {
+            id: 'child1.2',
+            text: 'Child 1.2',
+            object: { type: 'child', id: '1.2' },
+            canMatchFilter: true
+          }
+        ]
       },
       {
         id: 'folder2',
         text: 'Folder 2',
         object: { type: 'folder', id: '2' },
-        children: []
+        children: children2
       }
     ]
 
