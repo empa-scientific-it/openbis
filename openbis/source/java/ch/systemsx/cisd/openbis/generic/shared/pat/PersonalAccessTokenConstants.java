9b99fa7d6f52c5d1e86ac387c4f03f7868990dd5
pkupczyk <piotr.kupczyk@id.ethz.ch>
2022-09-20 10:07:34 +0200

    SSDM-11550 : Personal Access Tokens (PAT) - enable PATs by default

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/pat/PersonalAccessTokenConstants.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/shared/pat/PersonalAccessTokenConstants.java
@@ -7,7 +7,7 @@ public class PersonalAccessTokenConstants
 
     public static final String PERSONAL_ACCESS_TOKENS_ENABLED_KEY = "personal-access-tokens-enabled";
 
-    public static final boolean PERSONAL_ACCESS_TOKENS_ENABLED_DEFAULT = false;
+    public static final boolean PERSONAL_ACCESS_TOKENS_ENABLED_DEFAULT = true;
 
     public static final String PERSONAL_ACCESS_TOKENS_MAX_VALIDITY_PERIOD = "personal-access-tokens-max-validity-period";
 
