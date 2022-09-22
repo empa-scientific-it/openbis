404662ab030372ac3692f6dedf6eb1dce644216c
juanf <juanf@ethz.ch>
2022-09-22 17:12:50 +0200

    SSDM-13017 : Client side API Extensions

--- openbis_api/source/java/ch/ethz/sis/openbis/generic/asapi/v3/ApplicationServerAPIExtensions.java
+++ openbis_api/source/java/ch/ethz/sis/openbis/generic/asapi/v3/ApplicationServerAPIExtensions.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-public class ApplicationServerAPIExtension {
+public class ApplicationServerAPIExtensions {
     //
     // Facade Methods
     //
