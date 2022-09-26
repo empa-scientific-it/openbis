cc273b3f14515a5f86728cac1b67b54d9e900200
juanf <juanf@ethz.ch>
2022-09-26 09:53:34 +0200

    SSDM-13017 : Client side API Extensions

--- openbis_api/source/java/ch/ethz/sis/openbis/generic/asapi/v3/dto/server/ServerInformation.java
+++ openbis_api/source/java/ch/ethz/sis/openbis/generic/asapi/v3/dto/server/ServerInformation.java
@@ -1,5 +1,8 @@
 package ch.ethz.sis.openbis.generic.asapi.v3.dto.server;
 
+import ch.systemsx.cisd.base.annotation.JsonObject;
+
+@JsonObject("as.dto.server.ServerInformation")
 public interface ServerInformation {
 
     public static final String PERSONAL_ACCESS_TOKENS_MAX_VALIDITY_PERIOD = "personal-access-tokens-max-validity-period";
