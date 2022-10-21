450767e4e19f535496fbc0609810802d8f184b62
juanf <juanf@ethz.ch>
2022-10-21 13:02:48 +0200

    SSDM-13107 : ELN Storage: Always validate where boxes are stored if box name is given.

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/services/as-eln-lims-api/script.py
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/as/services/as-eln-lims-api/script.py
@@ -539,7 +539,7 @@ def isValidStoragePositionToInsertUpdate(context, parameters, sessionToken):
 
     # 4. IF $STORAGE.STORAGE_VALIDATION_LEVEL >= BOX
     # OPERATION_LOG.info("isValidStoragePositionToInsertUpdate - 4");
-    if storageValidationLevel == "BOX" or storageValidationLevel == "BOX_POSITION":
+    if storageBoxName is not None:
         # 4.1 Check that a box with the same name only exist on the given storage and rack position
         # OPERATION_LOG.info("isValidStoragePositionToInsertUpdate - 4.1");
         searchCriteriaOtherBox = SampleSearchCriteria();
@@ -554,6 +554,7 @@ def isValidStoragePositionToInsertUpdate(context, parameters, sessionToken):
             if (result.getProperty("$STORAGE_POSITION.STORAGE_CODE") != storageCode) or (result.getProperty("$STORAGE_POSITION.STORAGE_RACK_ROW") != storageRackRow) or (result.getProperty("$STORAGE_POSITION.STORAGE_RACK_COLUMN") != storageRackColumn):
                 raise UserFailureException("You entered the name of an already existing box in a different place - Box Name: " + str(storageBoxName) + " Given -> Storage Code: " + str(storageCode) + " Rack Row: " + str(storageRackRow) + " Rack Column: " + str(storageRackColumn) + " - Found -> Storage Code: " + result.getProperty("$STORAGE_POSITION.STORAGE_CODE") + " Rack Row: " + result.getProperty("$STORAGE_POSITION.STORAGE_RACK_ROW") + " Rack Column: " + result.getProperty("$STORAGE_POSITION.STORAGE_RACK_COLUMN"));
 
+    if storageValidationLevel == "BOX" or storageValidationLevel == "BOX_POSITION":
         # 4.2 The number of total different box names on the rack including the given one should be below $STORAGE.BOX_NUM
         # OPERATION_LOG.info("isValidStoragePositionToInsertUpdate - 4.2");
         searchCriteriaStorageRack = SampleSearchCriteria();
