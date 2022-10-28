868c8b152234db7b9a7dce2ad93386b9cfdb7ca6
vkovtun <victor.kovtun@gmail.com>
2022-10-28 11:46:42 +0200

    SSDM-12286 Implementing entry point.

--- openbis_ng_ui/src/core-plugins/openbis-ng-ui/1/as/services/xls-export/xls-export.py
+++ openbis_ng_ui/src/core-plugins/openbis-ng-ui/1/as/services/xls-export/xls-export.py
@@ -1,2 +1,42 @@
+from ch.ethz.sis.openbis.generic.server.xls.export import XLSExport, ExportablePermId, ExportableKind
+
+
 def process(context, parameters):
-    return None
+    method = parameters.get("method")
+    result = None
+
+    if method == "export":
+        result = export(context, parameters)
+
+    return result
+
+
+def export(context, parameters):
+    """
+        Excel export AS service.
+        :param context: Standard Openbis AS Service context object
+        :param parameters: Contains the following elements:
+                        {
+                            ids: [{
+                                exportable_kind: [SAMPLE_TYPE | EXPERIMENT_TYPE | DATASET_TYPE | VOCABULARY | SPACE |
+                                    PROJECT | SAMPLE | EXPERIMENT | DATASET] - entity kind to export
+                                perm_id: permId of the exportable
+                            }]
+
+                            export_referred: whether the referred entities should be exported as well
+                        }
+        :return: Openbis's execute operations result string. It should contain report on what was created.
+    """
+    try:
+        vocabularies = map(lambda id: ExportablePermId(ExportableKind.valueOf(id.get("exportable_kind")),
+                                                                              id.get("perm_id")),
+                           parameters.get("ids", {}))
+
+        session_token = context.getSessionToken()
+        api = context.getApplicationService()
+        xls_export = XLSExport()
+        bytes = xls_export.export("export.xlsx", api, session_token, vocabularies,
+                                  parameters.get("export_referred", False))
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+    return {"status": "OK", "result": bytes}
