eddfe2cd0cdbb4a033966e4501a45b830c522452
juanf <juanf@ethz.ch>
2022-12-07 16:44:03 +0100

    SSDM-13195 : Bugfix, allow cascade deletions

--- openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
+++ openbis/source/java/ch/systemsx/cisd/openbis/generic/server/dataaccess/db/DatabaseVersionHolder.java
@@ -26,7 +26,7 @@ public final class DatabaseVersionHolder
     /**
      * Current version of the database.
      */
-    private static final String DATABASE_VERSION = "189";
+    private static final String DATABASE_VERSION = "190";
 
     /** Current version of the database INDICES. */
     private static final String DATABASE_FULL_TEXT_SEARCH_DOCUMENT_VERSION = "002";
