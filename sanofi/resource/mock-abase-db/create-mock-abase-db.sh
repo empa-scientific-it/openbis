48dec8794ecf908b7e393aeab88870da22500d53
alaskowski <alaskowski@ethz.ch>
2023-01-19 10:13:13 +0100

    SSDM-13329 Removed sanofi project

--- sanofi/resource/mock-abase-db/create-mock-abase-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-USER=`whoami`
-ABASE_DB="abase_mock_db"
-
-psql -U postgres -c "drop database if exists $ABASE_DB;"
-psql -U postgres -c "create database $ABASE_DB with owner $USER template = template0 encoding = 'UNICODE'"
-psql -U $USER -d $ABASE_DB -f mock-abase-db.sql
