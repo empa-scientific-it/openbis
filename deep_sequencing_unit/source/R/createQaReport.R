df60dce04affbd11b19f9fb826e1dcaa6ae09b52
alaskowski <alaskowski@ethz.ch>
2023-01-20 09:14:37 +0100

    SSDM-13329 Moved deep_sequencing_unit and openbis_contrib to another repository: https://sissource.ethz.ch/openbis/openbis-private/openbis-dsu.ethz.ch

--- deep_sequencing_unit/source/R/createQaReport.R
+++
@@ -1,7 +0,0 @@
-require(multicore)
-require (ShortRead)
-args <- commandArgs(TRUE)
-fastq_file <- args[1]
-qa <- qa(getwd(),args[1], type="fastq")
-report_dest <- paste(fastq_file, "ShortRead_qa", sep="_")
-qa_report <- report (qa, dest=report_dest , type="html")
--- deep_sequencing_unit/source/bash/convert_template.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Usage: eland2wig <chromosomes> <trackname> <fragmentsize> <windowsize> <strand>
-eland2wig drosmel.genome Lane5 160 10 0 <s_5_export.txt | gzip - -9 >s_5.wig.gz
