292e9b9bcd31b158982fc4ec50e32bed3c1f5565
alaskowski <alaskowski@ethz.ch>
2023-01-20 11:34:16 +0100

    SSDM-13329 Moved openbis_benchmark to a new repository: https://sissource.ethz.ch/openbis/openbis-private/TOOLS/openbis-benchmark

--- openbis_benchmark/src/main/java/ch/ethz/sis/logging/LogFactoryFactory.java
+++
@@ -1,7 +0,0 @@
-package ch.ethz.sis.logging;
-
-public class LogFactoryFactory {
-    public LogFactory create(String logFactoryClass) throws Exception {
-        return (LogFactory) Class.forName(logFactoryClass).newInstance();
-    }
-}
