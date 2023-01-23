45037517349404cb9b85da1da395e8700958a39e
juanf <juanf@ethz.ch>
2023-01-23 13:21:29 +0100

    SSDM-13017 : Semantic Annotation methods on Facade

--- openbis_api/source/java/ch/ethz/sis/openbis/generic/OpenBISAPI.java
+++ openbis_api/source/java/ch/ethz/sis/openbis/generic/OpenBISAPI.java
@@ -135,6 +135,7 @@ import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.delete.PropertyTypeDele
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.fetchoptions.PropertyAssignmentFetchOptions;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.fetchoptions.PropertyTypeFetchOptions;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.id.IPropertyTypeId;
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.id.PropertyAssignmentPermId;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.id.PropertyTypePermId;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.search.PropertyAssignmentSearchCriteria;
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.property.search.PropertyTypeSearchCriteria;
@@ -1021,6 +1022,75 @@ public class OpenBISAPI {
         }
 
         return bestTokenFoundPermId;
+    }
+
+    /**
+     * This utility method provides a simplified API to create subject semantic annotations
+     *
+     */
+    public static SemanticAnnotationCreation getSemanticSubjectCreation(    EntityKind subjectEntityKind,
+                                                                            String subjectClass,
+                                                                            String subjectClassOntologyId,
+                                                                            String subjectClassOntologyVersion,
+                                                                            String subjectClassId) {
+        SemanticAnnotationCreation semanticAnnotationCreation = new SemanticAnnotationCreation();
+        // Subject: Type matching an ontology class
+        semanticAnnotationCreation.setEntityTypeId(new EntityTypePermId(subjectClass, subjectEntityKind));
+        // Ontology URL
+        semanticAnnotationCreation.setPredicateOntologyId(subjectClassOntologyId);
+        semanticAnnotationCreation.setDescriptorOntologyId(subjectClassOntologyId);
+        // Ontology Version URL
+        semanticAnnotationCreation.setPredicateOntologyVersion(subjectClassOntologyVersion);
+        semanticAnnotationCreation.setDescriptorOntologyVersion(subjectClassOntologyVersion);
+        // Ontology Class URL
+        semanticAnnotationCreation.setPredicateAccessionId(subjectClassId);
+        semanticAnnotationCreation.setDescriptorAccessionId(subjectClassId);
+        return semanticAnnotationCreation;
+    }
+
+    /**
+     * This utility method provides a simplified API to create predicate semantic annotations
+     *
+     */
+    public static SemanticAnnotationCreation getSemanticPredicateWithSubjectCreation( EntityKind subjectEntityKind,
+                                                                                      String subjectClass,
+                                                                                      String predicateProperty,
+                                                                                      String predicatePropertyOntologyId,
+                                                                                      String predicatePropertyOntologyVersion,
+                                                                                      String predicatePropertyId) {
+        SemanticAnnotationCreation semanticAnnotationCreation = new SemanticAnnotationCreation();
+        // Subject: Type matching an ontology class
+        // Predicate: Property matching an ontology class property
+        semanticAnnotationCreation.setPropertyAssignmentId(new PropertyAssignmentPermId(
+                new EntityTypePermId(subjectClass, subjectEntityKind),
+                new PropertyTypePermId(predicateProperty)));
+        // Ontology URL
+        semanticAnnotationCreation.setPredicateOntologyId(predicatePropertyOntologyId);
+        // Ontology Version URL
+        semanticAnnotationCreation.setPredicateOntologyVersion(predicatePropertyOntologyVersion);
+        // Ontology Property URL
+        semanticAnnotationCreation.setPredicateAccessionId(predicatePropertyId);
+        return semanticAnnotationCreation;
+    }
+
+    /**
+     * This utility method provides a simplified API to create predicate semantic annotations
+     *
+     */
+    public static SemanticAnnotationCreation getSemanticPredicateCreation( String predicateProperty,
+                                                                           String predicatePropertyOntologyId,
+                                                                           String predicatePropertyOntologyVersion,
+                                                                           String predicatePropertyId) {
+        SemanticAnnotationCreation semanticAnnotationCreation = new SemanticAnnotationCreation();
+        // Predicate: Property matching an ontology class property
+        semanticAnnotationCreation.setPropertyTypeId(new PropertyTypePermId(predicateProperty));
+        // Ontology URL
+        semanticAnnotationCreation.setPredicateOntologyId(predicatePropertyOntologyId);
+        // Ontology Version URL
+        semanticAnnotationCreation.setPredicateOntologyVersion(predicatePropertyOntologyVersion);
+        // Ontology Property URL
+        semanticAnnotationCreation.setPredicateAccessionId(predicatePropertyId);
+        return semanticAnnotationCreation;
     }
 
     //
