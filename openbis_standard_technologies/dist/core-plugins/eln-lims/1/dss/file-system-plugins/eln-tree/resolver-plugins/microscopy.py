f7e92fa3994dd1b935a9c2dc491a0fa05fb7df0a
felmer <franz-josef.elmer@id.ethz.ch>
2023-01-27 09:19:45 +0100

    SSDM-12929: hide certain data set types for microscopy in SFTP

---
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/file-system-plugins/eln-tree/resolver-plugins/microscopy.py
@@ -0,0 +1,4 @@
+acceptor.hideDataSetType("MICROSCOPY_IMG")
+acceptor.hideDataSetType("MICROSCOPY_IMG_OVERVIEW")
+acceptor.hideDataSetType("MICROSCOPY_IMG_THUMBNAIL")
+acceptor.hideDataSetType("MICROSCOPY_IMG_CONTAINER")
