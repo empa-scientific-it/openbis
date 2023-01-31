f51e3f1fb81baf972d1379fadc58d650538ae77e
felmer <franz-josef.elmer@id.ethz.ch>
2023-01-31 08:39:27 +0100

    SSDM-12929: hide only container data sets for microscopy

--- openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/file-system-plugins/eln-tree/resolver-plugins/microscopy.py
+++ openbis_standard_technologies/dist/core-plugins/eln-lims/1/dss/file-system-plugins/eln-tree/resolver-plugins/microscopy.py
@@ -1,4 +1,1 @@
-acceptor.hideDataSetType("MICROSCOPY_IMG")
-acceptor.hideDataSetType("MICROSCOPY_IMG_OVERVIEW")
-acceptor.hideDataSetType("MICROSCOPY_IMG_THUMBNAIL")
 acceptor.hideDataSetType("MICROSCOPY_IMG_CONTAINER")
