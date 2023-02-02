ff35f362e1bedd391720b1412a3d4fbc05294150
alaskowski <alaskowski@ethz.ch>
2023-02-02 09:23:12 +0100

    SSDM-13365: Added debugging log to test

--- test-app-openbis-installer/test_scu_microscopy.py
+++ test-app-openbis-installer/test_scu_microscopy.py
@@ -11,6 +11,7 @@ import systemtest.testcase
 import systemtest.util as util
 from systemtest.artifactrepository import GitArtifactRepository
 
+
 class TestCase(systemtest.testcase.TestCase):
 
     def execute(self):
@@ -26,10 +27,10 @@ class TestCase(systemtest.testcase.TestCase):
         for exampleName in sorted(os.listdir(testDataFolder)):
             if os.path.isdir("%s/%s" % (testDataFolder, exampleName)):
                 self.dropTestExample(openbisController, testDataFolder, exampleName)
-            
+
     def setUpAndStartOpenbis(self):
         util.printWhoAmI()
-        self.installOpenbis(technologies = ['microscopy'])
+        self.installOpenbis(technologies=['microscopy'])
         openbisController = self.createOpenbisController(databasesToDrop=['openbis', 'pathinfo'])
         self.setThumbnailResolutions(openbisController, ['256x256'])
         openbisController.setDssMaxHeapSize("4g")
@@ -39,7 +40,7 @@ class TestCase(systemtest.testcase.TestCase):
 
     def setThumbnailResolutions(self, openbisController, resolutions):
         path = "%s/servers/core-plugins/microscopy/2/dss/drop-boxes/MicroscopyDropbox/GlobalSettings.py" % openbisController.installPath
-        with open(path, "r")  as f:
+        with open(path, "r") as f:
             content = f.readlines()
         with open(path, "w") as f:
             for line in content:
@@ -52,12 +53,13 @@ class TestCase(systemtest.testcase.TestCase):
         if os.path.exists(testDataFolder):
             util.printAndFlush("Path exists as expected: %s" % testDataFolder)
         else:
-            self.fail("Test data folder missing: %s" % testDataFolder)
+            self.fail(
+                "Test data folder missing: %s - current path: %s" % (testDataFolder, os.getcwd()))
         return testDataFolder
-    
+
     def dropTestExample(self, openbisController, testDataFolder, exampleName):
         header = "\n/\\/\\/\\/\\/\\/\\ %s /\\/\\/\\/\\/\\/\\ %%s" % exampleName
-        util.printWhoAmI(template = header)
+        util.printWhoAmI(template=header)
         expectations = Expectations(openbisController, exampleName)
         destination = "%s/data/incoming-microscopy" % (openbisController.installPath)
         exampleFolder = "%s/%s" % (testDataFolder, exampleName)
@@ -67,33 +69,56 @@ class TestCase(systemtest.testcase.TestCase):
 
         print("numberOfPhysicalDataSets equals " + str(expectations.numberOfPhysicalDataSets))
         try:
-            openbisController.waitUntilDataSetRegistrationFinished(expectations.numberOfPhysicalDataSets, timeOutInMinutes = expectations.registrationTimeout)
-            self.waitUntilThumbnailGenerationTaskFinished(openbisController, expectations.numberOfThumbnailDataSets, timeOutInMinutes = expectations.registrationTimeout)
+            openbisController.waitUntilDataSetRegistrationFinished(
+                expectations.numberOfPhysicalDataSets,
+                timeOutInMinutes=expectations.registrationTimeout)
+            self.waitUntilThumbnailGenerationTaskFinished(openbisController,
+                                                          expectations.numberOfThumbnailDataSets,
+                                                          timeOutInMinutes=expectations.registrationTimeout)
             logInfo = self.getLogInfo(openbisController, exampleName)
-            print("Number of experiments created equals " + str(expectations.numberOfExperiments) + " " + str(logInfo.numberOfExperiments))
-            print("Number of samples created equals " + str(expectations.numberOfSamples) + " " + str(logInfo.numberOfSamples))
-            print("Number of data sets created equals " + str(expectations.numberOfDataSets) + " " + str(logInfo.numberOfDataSets))
-            print("Thumbnail generation time smaller " + str(expectations.thumbnailGenerationTime) + " " + str(logInfo.thumbnailTime))
-            print("Registration time smaller " + str(expectations.registrationTime) + " " + str(logInfo.jobTime))
+            print("Number of experiments created equals " + str(
+                expectations.numberOfExperiments) + " " + str(logInfo.numberOfExperiments))
+            print(
+                "Number of samples created equals " + str(expectations.numberOfSamples) + " " + str(
+                    logInfo.numberOfSamples))
+            print("Number of data sets created equals " + str(
+                expectations.numberOfDataSets) + " " + str(logInfo.numberOfDataSets))
+            print("Thumbnail generation time smaller " + str(
+                expectations.thumbnailGenerationTime) + " " + str(logInfo.thumbnailTime))
+            print("Registration time smaller " + str(expectations.registrationTime) + " " + str(
+                logInfo.jobTime))
         except Exception:
             logInfo = self.getLogInfo(openbisController, exampleName)
-            print("Number of experiments created equals " + str(expectations.numberOfExperiments) + " " + str(logInfo.numberOfExperiments))
-            print("Number of samples created equals " + str(expectations.numberOfSamples) + " " + str(logInfo.numberOfSamples))
-            print("Number of data sets created equals " + str(expectations.numberOfDataSets) + " " + str(logInfo.numberOfDataSets))
-            print("Thumbnail generation time smaller " + str(expectations.thumbnailGenerationTime) + " " + str(logInfo.thumbnailTime))
-            print("Registration time smaller " + str(expectations.registrationTime) + " " + str(logInfo.jobTime))
+            print("Number of experiments created equals " + str(
+                expectations.numberOfExperiments) + " " + str(logInfo.numberOfExperiments))
+            print(
+                "Number of samples created equals " + str(expectations.numberOfSamples) + " " + str(
+                    logInfo.numberOfSamples))
+            print("Number of data sets created equals " + str(
+                expectations.numberOfDataSets) + " " + str(logInfo.numberOfDataSets))
+            print("Thumbnail generation time smaller " + str(
+                expectations.thumbnailGenerationTime) + " " + str(logInfo.thumbnailTime))
+            print("Registration time smaller " + str(expectations.registrationTime) + " " + str(
+                logInfo.jobTime))
             raise Exception(sys.exc_info()[0])
 
-        self.assertEquals("Number of experiments created", expectations.numberOfExperiments, logInfo.numberOfExperiments)
-        self.assertEquals("Number of samples created", expectations.numberOfSamples, logInfo.numberOfSamples)
-        self.assertEquals("Number of data sets created", expectations.numberOfDataSets, logInfo.numberOfDataSets)
-        self.assertSmaller("Thumbnail generation time", expectations.thumbnailGenerationTime, logInfo.thumbnailTime)
+        self.assertEquals("Number of experiments created", expectations.numberOfExperiments,
+                          logInfo.numberOfExperiments)
+        self.assertEquals("Number of samples created", expectations.numberOfSamples,
+                          logInfo.numberOfSamples)
+        self.assertEquals("Number of data sets created", expectations.numberOfDataSets,
+                          logInfo.numberOfDataSets)
+        self.assertSmaller("Thumbnail generation time", expectations.thumbnailGenerationTime,
+                           logInfo.thumbnailTime)
         self.assertSmaller("Registration time", expectations.registrationTime, logInfo.jobTime)
 
-    def waitUntilThumbnailGenerationTaskFinished(self, openbisController, numberOfExpectedThumbnailDataSets, timeOutInMinutes):
+    def waitUntilThumbnailGenerationTaskFinished(self, openbisController,
+                                                 numberOfExpectedThumbnailDataSets,
+                                                 timeOutInMinutes):
         monitor = openbisController.createLogMonior(timeOutInMinutes)
         monitor.addNotificationCondition(util.RegexCondition('microscopy_thumbnails_creation'))
-        monitor.waitUntilEvent(util.RegexCondition("%s thumbnail data sets have been created" % numberOfExpectedThumbnailDataSets))
+        monitor.waitUntilEvent(util.RegexCondition(
+            "%s thumbnail data sets have been created" % numberOfExpectedThumbnailDataSets))
 
     def getLogInfo(self, openbisController, exampleName):
         logFolder = "%s/servers/datastore_server/log-registrations/succeeded" % openbisController.installPath
@@ -115,30 +140,29 @@ class TestCase(systemtest.testcase.TestCase):
                     initial_thumb_time = mktime(time.strptime(line[0:19], '%Y-%m-%d %H:%M:%S'))
                 if "Prepared registration" in line:
                     last_thumb_time = mktime(time.strptime(line[0:19], '%Y-%m-%d %H:%M:%S'))
-            
+
             logInfo.thumbnailTime = last_thumb_time - initial_thumb_time
-        util.printAndFlush(("Registration took %s seconds (%s seconds for thumbnail creation) " 
-                           + "to create %s experiments, %s samples, %s data sets") 
-                           % (logInfo.jobTime, logInfo.thumbnailTime, logInfo.numberOfExperiments, 
+        util.printAndFlush(("Registration took %s seconds (%s seconds for thumbnail creation) "
+                            + "to create %s experiments, %s samples, %s data sets")
+                           % (logInfo.jobTime, logInfo.thumbnailTime, logInfo.numberOfExperiments,
                               logInfo.numberOfSamples, logInfo.numberOfDataSets))
         return logInfo
 
     def getNumFromString(self, string):
         """Get the number at the end of a string."""
-    
+
         m = re.search(r'\d+$', string)
         if m is not None:
             return int(m.group())
         else:
             return 0
-    
+
     def getLogFile(self, logFolder, exampleName):
         for f in os.listdir(logFolder):
             if f.find(exampleName) > 0:
                 return "%s/%s" % (logFolder, f)
         raise Exception("No log file for example %s found in %s." % (exampleName, logFolder))
 
-        
 
 class Expectations(object):
     def __init__(self, openbisController, exampleName):
@@ -153,6 +177,7 @@ class Expectations(object):
         self.registrationTimeout = int(properties['registration-timeout-in-minutes']);
         self.thumbnailGenerationTime = int(properties['thumbnail-generation-time-in-seconds'])
         self.registrationTime = int(properties['registration-time-in-seconds'])
+
 
 class LogInfo(object):
     def __init__(self, exampleName):
