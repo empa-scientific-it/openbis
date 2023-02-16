d575cce3dd4686979904d62a3a58190d1203b577
alaskowski <alaskowski@ethz.ch>
2023-02-16 13:13:59 +0100

    SSDM-13365: Updated copyright comments in lib-image-readers

--- lib-image-readers/source/java/ch/systemsx/cisd/imagereaders/bioformats/BioFormatsImageProcessor.java
+++ lib-image-readers/source/java/ch/systemsx/cisd/imagereaders/bioformats/BioFormatsImageProcessor.java
@@ -1,24 +1,18 @@
-/**
-6   LOCI Plugins for ImageJ: a collection of ImageJ plugins including the
-7   Bio-Formats Importer, Bio-Formats Exporter, Bio-Formats Macro Extensions,
-8   Data Browser and Stack Slicer. Copyright (C) 2005-@year@ Melissa Linkert,
-9   Curtis Rueden and Christopher Peterson.
-10  
-11  This program is free software; you can redistribute it and/or modify
-12  it under the terms of the GNU General Public License as published by
-13  the Free Software Foundation; either version 2 of the License, or
-14  (at your option) any later version.
-15  
-16  This program is distributed in the hope that it will be useful,
-17  but WITHOUT ANY WARRANTY; without even the implied warranty of
-18  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-19  GNU General Public License for more details.
-20  
-21  You should have received a copy of the GNU General Public License
-22  along with this program; if not, write to the Free Software
-23  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-24  */
-
+/*
+ * Copyright ETH 2011 - 2023 Zürich, Scientific IT Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ch.systemsx.cisd.imagereaders.bioformats;
 
 import ij.process.ByteProcessor;
--- lib-image-readers/source/java/ch/systemsx/cisd/imagereaders/bioformats/FlexHelper.java
+++ lib-image-readers/source/java/ch/systemsx/cisd/imagereaders/bioformats/FlexHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 ETH Zuerich, CISD
+ * Copyright ETH 2011 - 2023 Zürich, Scientific IT Services
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
