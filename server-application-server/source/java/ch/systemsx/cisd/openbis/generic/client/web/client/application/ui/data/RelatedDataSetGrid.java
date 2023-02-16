e099e1f572994adeddecf7275f37cf70a4b45098
alaskowski <alaskowski@ethz.ch>
2023-02-16 15:33:53 +0100

    SSDM-13365: Updated copyright comments in server-application-server

--- server-application-server/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/ui/data/RelatedDataSetGrid.java
+++ server-application-server/source/java/ch/systemsx/cisd/openbis/generic/client/web/client/application/ui/data/RelatedDataSetGrid.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 ETH Zuerich, CISD
+ * Copyright ETH 2009 - 2023 Zürich, Scientific IT Services
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
  */
-
 package ch.systemsx.cisd.openbis.generic.client.web.client.application.ui.data;
 
 import ch.systemsx.cisd.openbis.generic.client.web.client.ICommonClientServiceAsync;
--- server-application-server/sourceTest/java/ch/ethz/sis/openbis/systemtest/asapi/v3/GetQueryDatabaseTest.java
+++ server-application-server/sourceTest/java/ch/ethz/sis/openbis/systemtest/asapi/v3/GetQueryDatabaseTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 ETH Zuerich, Scientific IT Services
+ * Copyright ETH 2020 - 2023 Zürich, Scientific IT Services
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package ch.ethz.sis.openbis.systemtest.asapi.v3;
 
 import static org.testng.Assert.assertEquals;
