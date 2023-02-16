051c3e4402c13cf23b2a0ec1ea0c3d5084c3de99
alaskowski <alaskowski@ethz.ch>
2023-02-16 10:28:15 +0100

    SSDM-13365: Updated copyright comments in app-openbis-installer

--- app-openbis-installer/source/java/InstallerVariableAccess.java
+++ app-openbis-installer/source/java/InstallerVariableAccess.java
@@ -1,6 +1,5 @@
-
 /*
- * Copyright 2019 ETH Zuerich, SIS
+ * Copyright ETH 2019 - 2023 Zürich, Scientific IT Services
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
