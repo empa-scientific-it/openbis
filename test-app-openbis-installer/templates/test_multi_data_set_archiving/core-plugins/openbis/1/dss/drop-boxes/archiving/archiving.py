8261ecfe5815a13ab489e19746c273088851e349
alaskowski <alaskowski@ethz.ch>
2023-02-17 12:16:35 +0100

    SSDM-13365: Updated copyright comments in test-app-openbis-installer project

--- test-app-openbis-installer/templates/test_multi_data_set_archiving/core-plugins/openbis/1/dss/drop-boxes/archiving/archiving.py
+++ test-app-openbis-installer/templates/test_multi_data_set_archiving/core-plugins/openbis/1/dss/drop-boxes/archiving/archiving.py
@@ -1,3 +1,17 @@
+#   Copyright ETH 2014 - 2023 Zürich, Scientific IT Services
+# 
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+#   
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 from ch.systemsx.cisd.openbis.dss.generic.shared import ServiceProvider
 from ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.id import DataSetPermId
 from ch.ethz.sis.openbis.generic.asapi.v3.dto.dataset.update import DataSetUpdate
