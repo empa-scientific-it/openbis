3128123a308cb5b53b2247a3980abbd28ebc6a41
alaskowski <alaskowski@ethz.ch>
2023-03-07 10:19:25 +0100

    SSDM-13300: obis-0.4.0

--- app-openbis-command-line/src/python/obis/__init__.py
+++ app-openbis-command-line/src/python/obis/__init__.py
@@ -14,6 +14,6 @@
 #
 __author__ = "ID SIS • ETH Zürich"
 __email__ = "openbis-support@id.ethz.ch"
-__version__ = "0.4.0rc6"
+__version__ = "0.4.0"
 
 from .dm import *
--- app-openbis-command-line/src/python/setup.py
+++ app-openbis-command-line/src/python/setup.py
@@ -31,7 +31,7 @@ data_files = [
 
 setup(
     name="obis",
-    version="0.4.0rc6",
+    version="0.4.0",
     description="Local data management with assistance from OpenBIS.",
     long_description=long_description,
     long_description_content_type="text/markdown",
