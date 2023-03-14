cd5d46022d6c72c2495346f55f804f3301efc785
juanf <juanf@ethz.ch>
2023-03-14 17:05:23 +0100

    SSDM-12402: Block ui during trashcan clearing

--- ui-eln-lims/src/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/TrashManager/TrashManagerView.js
+++ ui-eln-lims/src/core-plugins/eln-lims/1/as/webapps/eln-lims/html/js/views/TrashManager/TrashManagerView.js
@@ -31,6 +31,7 @@ function TrashManagerView(trashManagerController, trashManagerModel) {
 		var deleteAllBtn = $("<a>", { "class" : "btn btn-primary", "style" : "margin-top: 10px;", "id" : "empty-trash-btn"}).append("Empty Trash");
 		deleteAllBtn.click(function() {
 			Util.showWarning(deleteMessageMany, function() {
+			    Util.blockUINoMessage();
 				_this._trashManagerController.emptyTrash();
 			});
 		});
