842c4b3f25cfe86522dcf29d24e033f87aa34fc6
alaskowski <alaskowski@ethz.ch>
2023-03-22 09:57:09 +0100

    SSDM-13462: Fixed test-get after api version update

--- test-api-openbis-javascript/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/test/test-get.js
+++ test-api-openbis-javascript/servers/common/core-plugins/tests/1/as/webapps/openbis-v3-api-test/html/test/test-get.js
@@ -859,7 +859,7 @@ define([ 'jquery', 'underscore', 'openbis', 'test/openbis-execute-operations', '
 			c.createFacadeAndLogin().then(function(facade) {
 				return facade.getServerInformation().then(function(serverInformation) {
 					c.assertTrue(serverInformation != null);
-					c.assertEqual(serverInformation["api-version"], "3.5", "api-version");
+					c.assertEqual(serverInformation["api-version"], "3.6", "api-version");
 					c.assertEqual(serverInformation["project-samples-enabled"], "true", "project-samples-enabled");
 					c.finish();
 				});
