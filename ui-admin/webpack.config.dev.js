3cd6588557642b5c80e576f00e108aaee469feb8
pkupczyk <piotr.kupczyk@id.ethz.ch>
2023-04-04 15:57:09 +0200

    SSDM-13463 : Export selection of fields from tables - UI part - in development mode serve the admin ui at '/admin' path and 9999 port

--- ui-admin/webpack.config.dev.js
+++ ui-admin/webpack.config.dev.js
@@ -12,7 +12,7 @@ module.exports = {
 
   devServer: {
     host: '0.0.0.0',
-    port: 8124,
+    port: 9999,
     https: false,
     proxy: {
       '/openbis': {
@@ -23,7 +23,7 @@ module.exports = {
       }
     },
     devMiddleware: {
-      publicPath: '/ng-ui-path/'
+      publicPath: '/admin/'
     },
     static: {
       directory: './src/js'
