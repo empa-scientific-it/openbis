840b58a02ebe7583179a5ca86b38f57297b59db2
alaskowski <alaskowski@ethz.ch>
2023-05-12 15:52:53 +0200

    SSDM-13524: added setProperty methods to JS API

--- api-openbis-javascript/src/v3/as/dto/material/update/MaterialUpdate.js
+++ api-openbis-javascript/src/v3/as/dto/material/update/MaterialUpdate.js
@@ -35,6 +35,96 @@ define([ "stjs", "as/dto/common/update/IdListUpdateValue" ], function(stjs, IdLi
 		prototype.setProperties = function(properties) {
 			this.properties = properties;
 		};
+		prototype.getIntegerProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setIntegerProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getVarcharProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setVarcharProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getMultilineVarcharProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setMultilineVarcharProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getRealProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setRealProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getTimestampProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setTimestampProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getBooleanProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setBooleanProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getControlledVocabularyProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setControlledVocabularyProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getSampleProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setSampleProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getHyperlinkProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setHyperlinkProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getXmlProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setXmlProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getIntegerArrayProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setIntegerArrayProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getRealArrayProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setRealArrayProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getStringArrayProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setStringArrayProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getTimestampArrayProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setTimestampArrayProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
+        prototype.getJsonProperty = function(propertyName) {
+            return this.getProperty(propertyName);
+        };
+        prototype.setJsonProperty = function(propertyName, propertyValue) {
+            this.setProperty(propertyName, propertyValue);
+        };
 		prototype.getTagIds = function() {
 			return this.tagIds;
 		};
