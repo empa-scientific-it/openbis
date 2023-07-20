a916d24392fbc2c97b6fc8c3d4347e150e532e75
marcodeltufo <marco.deltufo@exact-lab.it>
2023-07-20 09:13:27 +0200

    added bash syntax

--- docs/user-documentation/advance-features/test.sh
+++ docs/user-documentation/advance-features/test.sh
@@ -8,8 +8,8 @@ do
         if [[ $(($i % 2)) -ne 0 ]]
         then
             echo "${line_number}: ${line}"
-            sed -i "${line_number}s/pythonget/get/" ./${1}
-            sed -i "${line_number}s/\`\`\`/\`\`\`bash/" ./${1}
+            #sed -i "${line_number}s/bashbash/bash/" ./${1}
+            #sed -i "${line_number}s/\`\`\`/\`\`\`bash/" ./${1}
         fi
         i=$((i+1))
     fi
