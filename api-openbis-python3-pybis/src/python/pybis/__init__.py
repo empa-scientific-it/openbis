1bd3f5d4af5ba6c6c4aaf2db37ada39588d47de6
alaskowski <alaskowski@ethz.ch>
2023-08-09 11:23:30 +0200

    SSDM-13838: PyBIS-1.36.1rc2

--- api-openbis-python3-pybis/src/python/pybis/__init__.py
+++ api-openbis-python3-pybis/src/python/pybis/__init__.py
@@ -15,7 +15,7 @@
 name = "pybis"
 __author__ = "ID SIS • ETH Zürich"
 __email__ = "openbis-support@id.ethz.ch"
-__version__ = "1.36.1rc1"
+__version__ = "1.36.1rc2"
 
 from . import pybis
 from .pybis import DataSet
