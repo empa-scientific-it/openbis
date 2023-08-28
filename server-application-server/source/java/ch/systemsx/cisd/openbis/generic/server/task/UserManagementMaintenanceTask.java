abfb2d87106f60b6a3a44431ff7e16708a1a44b0
juanf <juanf@ethz.ch>
2023-08-28 13:51:31 +0200

    SSDM-13421: trying to remove patches in favour of true bugfixes

--- server-application-server/source/java/ch/systemsx/cisd/openbis/generic/server/task/UserManagementMaintenanceTask.java
+++ server-application-server/source/java/ch/systemsx/cisd/openbis/generic/server/task/UserManagementMaintenanceTask.java
@@ -285,7 +285,6 @@ public class UserManagementMaintenanceTask extends AbstractGroupMaintenanceTask
     private UserManager createUserManager(UserManagerConfig config, Log4jSimpleLogger logger, UserManagerReport report)
     {
         UserManager userManager = createUserManager(logger, report);
-        userManager.setReuseHomeSpace(config.getReuseHomeSpace());
         userManager.setGlobalSpaces(config.getGlobalSpaces());
         userManager.setInstanceAdmins(config.getInstanceAdmins());
         try
