f7f7743d1531a088419fa6f7426e771d2844f8e5
pkupczyk <piotr.kupczyk@id.ethz.ch>
2023-09-06 15:15:12 +0200

    Revert "SSDM-13898 : OTRS ID#18663960 - Publish openBIS JS API on NPM - proof of concept of bundling V3 JS API facade and all dtos into one package"
    This reverts commit 2ccd1b488dd23db53e3ca7f9751437ffe5e68b5d.

--- api-openbis-javascript/src/v3/util/Json.js
+++ api-openbis-javascript/src/v3/util/Json.js
@@ -29,7 +29,7 @@ define([ 'jquery', 'underscore' ], function(jquery, _) {
 		var moduleNames = Object.keys(types).map(function(type) {
 			return typeToModuleName(type);
 		});
-		loadV3APIModule(moduleNames, function() {
+		require(moduleNames, function() {
 			var moduleMap = {};
 
 			for (var i = 0; i < arguments.length; i++) {
