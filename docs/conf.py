d50ccc90085afd71da58076ca2d58eb31b4ef22d
Marco Del Tufo <marco.deltufo@exact-lab.it>
2023-09-13 07:49:43 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ html_theme_options = {
         'logo_only': True,
         'display_version': False,
 }
-#extensions = ['myst_parser']
+extensions = ['myst_parser']
 source_suffix = ['.rst', '.md']
 def setup(app):
     app.add_css_file('my_theme.css')
