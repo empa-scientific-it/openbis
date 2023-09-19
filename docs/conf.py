4c69ac8642f46328dbd5cb5f116be4fafcdaa3d1
Marco Del Tufo <marco.deltufo@exact-lab.it>
2023-09-19 13:51:56 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 #import sphinx_rtd_theme
-
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'default'
+html_theme = "sphinx_rtd_theme"
+#html_theme = 'default'
 html_static_path = ['_static']
 html_logo = "img/openBIS.png"
 html_theme_options = {
