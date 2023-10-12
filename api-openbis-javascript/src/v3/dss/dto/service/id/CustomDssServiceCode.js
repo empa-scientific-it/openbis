0004e14a90c8b69ea87c4ac403aab042a01597cf
alaskowski <alaskowski@ethz.ch>
2023-10-12 18:32:58 +0200

    BIS-711: fixes to js-api for execution of custom dss scripts

--- api-openbis-javascript/src/v3/dss/dto/service/id/CustomDssServiceCode.js
+++ api-openbis-javascript/src/v3/dss/dto/service/id/CustomDssServiceCode.js
@@ -7,7 +7,7 @@ define([ "stjs", "as/dto/common/id/ObjectPermId", "dss/dto/service/id/ICustomDSS
 		ObjectPermId.call(this, code);
 	};
 	stjs.extend(CustomDSSServiceCode, ObjectPermId, [ ObjectPermId, ICustomDSSServiceId ], function(constructor, prototype) {
-		prototype['@type'] = 'dss.dto.service.id.CustomDSSServiceCode';
+		prototype['@type'] = 'dss.dto.service.id.CustomDssServiceCode';
 		constructor.serialVersionUID = 1;
 	}, {});
 	return CustomDSSServiceCode;
