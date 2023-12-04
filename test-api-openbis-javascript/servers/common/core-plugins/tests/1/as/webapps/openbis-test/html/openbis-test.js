d3d9cc32d67f46a43a59da9ccc8514592f5baa7b
pkupczyk <piotr.kupczyk@id.ethz.ch>
2023-12-04 11:08:16 +0100

    SSDM-14137 : ELN Storage : Make Space Sample Positions be part of a Storage Position Collection - fix JS tests

--- test-api-openbis-javascript/servers/common/core-plugins/tests/1/as/webapps/openbis-test/html/openbis-test.js
+++ test-api-openbis-javascript/servers/common/core-plugins/tests/1/as/webapps/openbis-test/html/openbis-test.js
@@ -1082,7 +1082,7 @@ test("searchForExperiments() withModifierUserId", function() {
 test("listProjects()", function() {
 	createFacadeAndLogin(function(facade) {
 		facade.listProjects(function(response) {
-			assertObjectsCount(response.result, 19);
+			assertObjectsCount(response.result, 20);
 			assertObjectsWithCodes(response.result, [ "BACTERIA" ,"CELL_LINES" ,"DEFAULT_PROJECT" ,"FLIES" ,"ORDERS"
 			,"PLANTS" ,"PLASMIDS" ,"POLYNUCLEOTIDES" ,"PRODUCTS" ,"PROTOCOLS" ,"PUBLIC_REPOSITORIES" ,"REAGENTS"
 			,"REQUESTS" ,"SCREENING-EXAMPLES" ,"STORAGES" ,"STORAGE_POSITIONS", "SUPPLIERS" ,"TEMPLATES" ,"TEST-PROJECT" ,"YEASTS" ]);
